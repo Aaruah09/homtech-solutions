44eb8d022f35c04110ca60ae601c1a71a926c6f8
Aaruah09 <139034068+Aaruah09@users.noreply.github.com>
2025-04-11 17:07:01 +0530

    Fixed App.jsx content

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
-<Insert Updated App Code from Canvas Here>
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to HomTechSolutions</h1>
+      <p>This is the homepage!</p>
+    </div>
+  );
+}
+
+export default App;
